aceecc3ba537a2071dedef645981befc33f7476b
Alex Sandro <geniofuturo@gmail.com>
2012-07-23 00:19:57 -0300

    adicionado script inicializador da aplicação.

---
+++ BaixeAssista.py
@@ -0,0 +1,28 @@
+# -*- coding: ISO-8859-1 -*-
+import os, sys
+
+from django.core.management import execute_manager
+os.environ['DJANGO_SETTINGS_MODULE'] = "main.settings"
+
+import wx
+import main
+from main.exeapp import *
+from main.exeapp.window import mainWin
+
+def run():
+	if len(sys.argv) > 1:
+		try:
+			execute_manager( main.settings )
+		except Exception, err:
+			print "ManagerErr: %s"%err
+		exit(0)
+	# -----------------------------------------------
+	
+	app = wx.App(False) # arg: False não redirecionar
+	mainWin.BaixeAssistaWin()
+	app.MainLoop()
+
+
+# ---------------------------------------------------
+if __name__== "__main__":
+	run()
