214ed77588f8963fd42842449fabb4a2dfd9fe14
Alex Sandro <geniofuturo@gmail.com>
2012-07-28 17:24:00 -0300

    colocando registros de models trys, por causa de exceção gerada ao registrar duas vezes o mesmo model.

--- main/exeapp/admin.py
+++ main/exeapp/admin.py
@@ -1,6 +1,9 @@
 from django.contrib.admin import site
 from models import Url, Browser, Resume
 
-site.register( Url )
-site.register( Browser )
-site.register( Resume )
+try: site.register( Url )
+except: pass
+try: site.register( Browser )
+except: pass
+try: site.register( Resume )
+except: pass 
