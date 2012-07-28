f0fce53e34928b05cfbc7bb5dd582900ae729f12
Alex Sandro <geniofuturo@gmail.com>
2012-07-28 18:23:45 -0300

    renomeando arquivos e pastas.

--- main/exeapp/__init__.py
+++
@@ -1,8 +0,0 @@
-import models
-import views
-import admin
-import browser
-import jwPlayer
-import manager
-import gerador
-import window
--- main/exeapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.admin import site
-from models import Url, Browser, Resume
-
-try: site.register( Url )
-except: pass
-try: site.register( Browser )
-except: pass
-try: site.register( Resume )
-except: pass 
