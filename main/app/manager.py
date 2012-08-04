43e78ba1a6b60347787a12c9a71f4b9806742ac4
Alex Sandro <geniofuturo@gmail.com>
2012-08-04 18:09:04 -0300

    atualizando versão do programa v0.1.9.

--- main/app/manager.py
+++ main/app/manager.py
@@ -1,6 +1,6 @@
 # -*- coding: ISO-8859-1 -*-
 ## guarda a versão do programa.
-PROGRAM_VERSION = "0.1.8"
+PROGRAM_VERSION = "0.1.9"
 PROGRAM_SYSTEM = {"Windows": "oswin", "Linux": "oslinux"}
 
 import sys
