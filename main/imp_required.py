5411c6d14365b91beeb3d2cdbdf2cdfaf2aa7b26
Alex Sandro <geniofuturo@gmail.com>
2012-10-27 01:02:26 -0200

    adicionando novo importe de dependência do programa compilado.

--- main/imp_required.py
+++ main/imp_required.py
@@ -1,3 +1,4 @@
 from django.template import Context, Template, loader, defaulttags, defaultfilters, loader_tags
 from django.contrib.messages.storage import fallback
+from django.core.mail.backends import smtp
 from app import *
