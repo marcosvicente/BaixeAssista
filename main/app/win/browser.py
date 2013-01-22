828de0ac7c3ca850ea5715e0234d4ce1f29fc7de
Alex Sandro <Alex Sandro@FireDragon>
2013-01-22 00:04:37 -0200

    definindo o sistema básico de navegação.

--- main/app/win/browser.py
+++ main/app/win/browser.py
@@ -4,11 +4,13 @@ from PySide import QtCore
 from PySide import QtGui
 from PySide import QtWebKit
 from main import settings
+
 import main.environ
 main.environ.setup((__name__ == "__main__"))
 
 
 class Browser (QtGui.QWidget):
+    searchEngine = "http://www.google.com/"
     
     def __init__(self, *arg):
         super(Browser, self).__init__(*arg)
@@ -19,10 +21,13 @@ class Browser (QtGui.QWidget):
         self.webView.settings().setAttribute(QtWebKit.QWebSettings.PluginsEnabled, True)
         self.webView.page().setLinkDelegationPolicy(QtWebKit.QWebPage.DelegateAllLinks)
         
-        self.webView.load(QtCore.QUrl("http://www.youtube.com/"))
+        self.current ="http://www.youtube.com/"
+        self.webView.load( QtCore.QUrl(self.current) )
         
-        self.webView.linkClicked.connect( self.loadLinkClicked )
+        self.webView.linkClicked.connect( self.onLinkClicked )
         self.webView.loadStarted.connect( self.onPageLoad )
+        self.webView.urlChanged.connect( self.onChangeUrl )
+        
         self.webView.show()
         
         vBox.addLayout( self._createToolbar() )
@@ -31,16 +36,26 @@ class Browser (QtGui.QWidget):
         self.setLayout( vBox )
         self.show()
         
-    def loadLinkClicked(self, url):
+    def onLinkClicked(self, url):
         self.webView.load( url )
-        print "Clicked: " + url.toString()
+        
+    def loadPage(self):
+        self.webView.load( QtCore.QUrl(self.location.currentText()) )
         
     def onPageLoad(self):
-        print "Loading: " + self.webView.url().toString()
+        url = self.webView.url().toString()
+    
+    def onChangeUrl(self, url):
+        self.current = url.toString()
+        self.location.setEditText( self.current )
+        self.location.setToolTip(self.current)
         
     def _createToolbar(self):
-        self.edit = QtGui.QLineEdit(self)
-        self.edit.show()
+        self.location = QtGui.QComboBox(self)
+        self.location.addItem( self.current )
+        self.location.activated.connect(self.loadPage)
+        self.location.setEditable(True)
+        self.location.show()
         
         ## Back button
         btnBack = QtGui.QPushButton(self)
@@ -67,26 +82,40 @@ class Browser (QtGui.QWidget):
         btnRefresh.show()
         
         ## Refresh button
-        btnGo = QtGui.QPushButton(self)
+        btnSearch = QtGui.QPushButton(self)
         path = os.path.join(settings.IMAGES_DIR, "btnsearch-blue.png")
-        btnGo.setIcon(QtGui.QIcon(path))
-        btnGo.setToolTip("<b>carregar</b>")
-        btnGo.clicked.connect( self.loadPage )
-        btnGo.show()
+        btnSearch.setIcon(QtGui.QIcon(path))
+        btnSearch.setToolTip("<b>pesquisar</b>")
+        btnSearch.clicked.connect(lambda: self.webView.load(self.searchEngine))
+        btnSearch.show()
+        
+        ## Add url button
+        btnNewUrl = QtGui.QPushButton(self)
+        path = os.path.join(settings.IMAGES_DIR, "btnplus-blue.png")
+        btnNewUrl.setIcon(QtGui.QIcon(path))
+        btnNewUrl.setToolTip("<b>adicionar nova url</b>")
+        #btnNewUrl.clicked.connect()
+        btnNewUrl.show()
+        
+        ## Add url button
+        btnDelUrl = QtGui.QPushButton(self)
+        path = os.path.join(settings.IMAGES_DIR, "btnminus-blue.png")
+        btnDelUrl.setIcon(QtGui.QIcon(path))
+        btnDelUrl.setToolTip("<b>remover url existente</b>")
+        #btnDelUrl.clicked.connect()
+        btnDelUrl.show()
         
         hBoxLayout = QtGui.QHBoxLayout()
         hBoxLayout.addWidget(btnBack)
         hBoxLayout.addWidget(btnForward)
         hBoxLayout.addWidget(btnRefresh)
         
-        hBoxLayout.addWidget(self.edit)
-        hBoxLayout.addWidget(btnGo)
+        hBoxLayout.addWidget(self.location, 1)
+        hBoxLayout.addWidget( btnSearch )
+        hBoxLayout.addWidget( btnNewUrl )
+        hBoxLayout.addWidget( btnDelUrl )
         return hBoxLayout
-    
-    def loadPage(self):
-        url = QtCore.QUrl(self.edit.text())
-        self.webView.load(url)
-    
+        
 
 ## ----------------------------------------------------------------
 if __name__ == '__main__':
