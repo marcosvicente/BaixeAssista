f4c1fc8feab3c7c77dee83e1205ffd238bc883d6
Alex Sandro <Alex Sandro@FireDragon>
2013-02-11 21:25:12 -0200

    definindo o sistema de carregamento do idioma com base no sistema operacional.

--- BaixeAssista.py
+++ BaixeAssista.py
@@ -9,21 +9,34 @@ from main.app.win import loader
 from main.app.util import base
 from main import settings
 from main import imps
+import locale
 
-sv = Server()
-sv.start()
+server = Server()
+server.start()
 
 app = QtGui.QApplication(sys.argv)
 
-base.trans_install()
+trans = QtCore.QTranslator()
+base.trans_install( loader.Loader.config )
 
-# pesquisando por todos os arquivo de tradução da ui.
-filename = "en_%s-py.qm" % loader.Loader.config["Lang"]["code"]
-filepath = os.path.join(settings.INTERFACE_DIR, "i18n", filename)
+code, encoding = locale.getdefaultlocale()
+userCode = loader.Loader.config["Lang"]["code"]
+booted = loader.Loader.config["Window"].as_bool("booted")
+i18nDir = os.path.join(settings.INTERFACE_DIR, "i18n")
 
-translator = QtCore.QTranslator()
-print "TL: ", translator.load( filepath )
-app.installTranslator(translator)
+if not booted:
+    filename = "en_US_%s.qm" % (code if not code is None else userCode)
+    filepath = os.path.join(i18nDir, filename)
+    
+    code = code if trans.load(filepath) else userCode
+    
+    loader.Loader.config["Window"]["booted"] = True
+    loader.Loader.config["Lang"]["code"] = code
+else:
+    filepath = os.path.join(i18nDir, "en_US_%s.qm" %userCode)
+    trans.load( filepath )
+    
+app.installTranslator(trans)
 
 mw = loader.Loader()
 mw.show()
