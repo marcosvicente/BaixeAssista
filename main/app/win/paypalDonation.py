a034d64a5783af24e6a42671ef328654b1ed9db0
Alex Sandro <Alex Sandro@FireDragon>
2013-02-11 17:54:15 -0200

    adicionando diálogo para pedido de doações.

---
+++ main/app/win/paypalDonation.py
@@ -0,0 +1,115 @@
+# coding: utf-8
+import sys, os
+from PySide import QtCore, QtGui
+from PySide.QtWebKit import QWebView
+from uiPaypalDonation import Ui_Dialog
+from django.template import Context, Template, loader
+from stopRefreshButton import StopRefreshButton
+from main import settings
+## --------------------------------------------------------------------------
+
+class DialogDonate(QtGui.QDialog):
+    template = "paypalDoar.html"
+    
+    def __init__(self, parent=None):
+        super(DialogDonate, self).__init__(parent)
+        self.uiDialog = Ui_Dialog()
+        self.uiDialog.setupUi(self)
+        
+        self.userName = os.environ.get("USERNAME",self.tr("User"))
+        
+        boxLayout = QtGui.QVBoxLayout()
+        self.btnFrame.setLayout(boxLayout)
+        
+        self.btnSR = StopRefreshButton()
+        self.btnSR.clicked.connect( self.onStopRefresh )
+        boxLayout.addWidget(self.btnSR, 0, 0)
+        
+        self.webView = QWebView( self.webViewFrame)
+        self.webView.loadStarted.connect(self.onPageLoad)
+        self.webView.loadFinished.connect(self.onPageFinished)
+        self.webView.loadProgress.connect(self.onProgress)
+        self.webView.urlChanged.connect(self.onChangeUrl)
+        
+        self.webView.setHtml(self.getTemplateHtml({"msg": self.getDevMsg()}))
+        self.webViewFrame.layout().addWidget( self.webView )
+        
+        self.btnClose.clicked.connect(self.close)
+        
+    def getDevMsg(self):
+        msg = self.tr("Hello! {username}<br/><br/>"
+            "Now you have the opportunity to contribute to the development of this project.<br/>"
+            "Donate the amount you can with the system palypal.<br/><br/>"
+            "Suggested Value: $ 5.00<br/><br/>"
+            "Click the donate button to start the donation process.<br/>"
+            "If you do not have a paypal account, you can do by clicking the donate button.<br/><br/>"
+            "You can disable this message by checking the box at the bottom of the window.<br/>"
+            "You can also donate at any time by clicking the menu about.<br/>"
+            "Anyway, Thanks for attention.<br/>")
+        return msg.format(username=self.userName)
+    
+    def onPageLoad(self):
+        self.address.setText(self.webView.url().toString())
+        self.info.setText(self.tr("Loading..."))
+        self.btnSR.setStopState()
+        
+    def onStopRefresh(self):
+        if self.btnSR["state"] == "refresh":
+            self.webView.reload()
+        else:
+            self.webView.stop()
+            
+    def onPageFinished(self, url):
+        self.info.setText(self.tr("..."))
+        self.btnSR.setRefreshState()
+        
+    def onProgress(self, progress):
+        self.info.setText(self.tr("Loading...")+(" %d%%"%progress))
+    
+    def onChangeUrl(self, url):
+        self.address.setToolTip(self.webView.url().toString())
+        self.uiDialog.address.setText(url.toString())
+        
+    @classmethod
+    def getTemplateHtml(cls, params):
+        try: tmpl = loader.get_template(cls.template)
+        except: tmpl = loader.find_template(cls.template, dirs=(settings.TEMPLATE_PATH,))[0]
+        return tmpl.render(Context({"params": params}))
+    
+    @property
+    def btnFrame(self):
+        return self.uiDialog.btnFrame
+    
+    @property
+    def webViewFrame(self):
+        return self.uiDialog.webViewFrame
+    
+    @property
+    def info(self):
+        return self.uiDialog.info
+    
+    @property
+    def address(self):
+        return self.uiDialog.address
+    
+    @property
+    def donationBoxStatus(self):
+        return self.uiDialog.donationBoxStatus
+    
+    @property
+    def btnClose(self):
+        return self.uiDialog.btnClose
+    
+## ----------------------------------------------------------------
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    
+    win = DialogDonate()
+    win.show()
+    
+    sys.exit(app.exec_())
+    
+    
+    
+    
+    
---
+++ main/app/win/uiPaypalDonation.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'uiPaypalDonation.ui'
+#
+# Created: Mon Feb 11 17:24:42 2013
+#      by: pyside-uic 0.2.14 running on PySide 1.1.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide import QtCore, QtGui
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(640, 400)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("../../../../../../.designer/images/movies.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        Dialog.setWindowIcon(icon)
+        Dialog.setModal(True)
+        self.verticalLayout = QtGui.QVBoxLayout(Dialog)
+        self.verticalLayout.setSpacing(5)
+        self.verticalLayout.setContentsMargins(5, 5, 5, 5)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setSpacing(5)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.btnFrame = QtGui.QFrame(Dialog)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.btnFrame.sizePolicy().hasHeightForWidth())
+        self.btnFrame.setSizePolicy(sizePolicy)
+        self.btnFrame.setAutoFillBackground(False)
+        self.btnFrame.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.btnFrame.setFrameShadow(QtGui.QFrame.Raised)
+        self.btnFrame.setObjectName("btnFrame")
+        self.horizontalLayout.addWidget(self.btnFrame)
+        self.address = QtGui.QLineEdit(Dialog)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.address.sizePolicy().hasHeightForWidth())
+        self.address.setSizePolicy(sizePolicy)
+        self.address.setObjectName("address")
+        self.horizontalLayout.addWidget(self.address)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.webViewFrame = QtGui.QFrame(Dialog)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(1)
+        sizePolicy.setHeightForWidth(self.webViewFrame.sizePolicy().hasHeightForWidth())
+        self.webViewFrame.setSizePolicy(sizePolicy)
+        self.webViewFrame.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.webViewFrame.setFrameShadow(QtGui.QFrame.Raised)
+        self.webViewFrame.setObjectName("webViewFrame")
+        self.verticalLayout_2 = QtGui.QVBoxLayout(self.webViewFrame)
+        self.verticalLayout_2.setSpacing(5)
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.verticalLayout.addWidget(self.webViewFrame)
+        self.horizontalLayout_2 = QtGui.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.info = QtGui.QLabel(Dialog)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.info.sizePolicy().hasHeightForWidth())
+        self.info.setSizePolicy(sizePolicy)
+        self.info.setObjectName("info")
+        self.horizontalLayout_2.addWidget(self.info)
+        self.btnClose = QtGui.QPushButton(Dialog)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.btnClose.sizePolicy().hasHeightForWidth())
+        self.btnClose.setSizePolicy(sizePolicy)
+        self.btnClose.setObjectName("btnClose")
+        self.horizontalLayout_2.addWidget(self.btnClose)
+        self.donationBoxStatus = QtGui.QCheckBox(Dialog)
+        self.donationBoxStatus.setObjectName("donationBoxStatus")
+        self.horizontalLayout_2.addWidget(self.donationBoxStatus)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "Make a donation", None, QtGui.QApplication.UnicodeUTF8))
+        self.info.setText(QtGui.QApplication.translate("Dialog", "...", None, QtGui.QApplication.UnicodeUTF8))
+        self.btnClose.setText(QtGui.QApplication.translate("Dialog", "Close", None, QtGui.QApplication.UnicodeUTF8))
+        self.donationBoxStatus.setText(QtGui.QApplication.translate("Dialog", "Do not show this message again.", None, QtGui.QApplication.UnicodeUTF8))
+
