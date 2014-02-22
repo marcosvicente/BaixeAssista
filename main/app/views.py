3bacf6fa5d206e6de88f295090e1ff4a66797318
alex <geniofuturo@gmail.com>
2014-02-21 22:28:41 -0300

    update

--- main/app/views.py
+++ main/app/views.py
@@ -1,55 +1,46 @@
 # Create your views here.
-from django.http import HttpResponse
-from django.shortcuts import render_to_response
-from main import settings
 import urllib
-import os
-from django.template import Context, Template, loader
 
-# --------------------------------------------------------------------------------------
-def streamLoader( request ):
-	if not request.manage:
-		return HttpResponse("waiting...")
-	
-	manage = request.manage
-	videoManager = manage.videoManager
-	querystr = request.GET
-	
-	seekpos = querystr.get("start",0)
-	
-	try: seekpos = long( seekpos )
-	except: seekpos = videoManager.get_relative_mp4( seekpos )
-	
-	if seekpos > 0 and manage.videoManager.suportaSeekBar():
-		manage.setRandomRead( seekpos )
-		statuscode = 206
-	else:
-		manage.reloadSettings()
-		statuscode = 200
-	
-	seekpos = long(seekpos)
-	filename = manage.getVideoTitle()
-	filename = filename.encode("utf-8","ignore")
-	videoSize = manage.getVideoSize()
-	videoExt = manage.getVideoExt()
-	
-	response = HttpResponse(manage.get_streamer(), status = statuscode)
-	response["Content-Type"] = "video/%s" %videoExt
-	response["Content-Length"] = videoSize
-	response["Content-Transfer-Encoding"] = "binary"
-	response['Content-Disposition'] = 'attachment; filename=%s'%filename
-	response["Accept-Ranges"] = "bytes"
-	return response
-
-def playerLoader(request):
-	params = request.GET.copy()
-	for key in params: params[key] = urllib.unquote_plus(params[key])
-	return render_to_response(params["template"], {"params": params}, 
-							  mimetype="text/html")
+from django.http import HttpResponse, StreamingHttpResponse
+from django.shortcuts import render_to_response
 
 
+def stream_loader(request):
+    if not request.manage:
+        return HttpResponse("waiting...")
+
+    manage = request.manage
+    video_manager = manage.videoManager
+    seek_pos = request.GET.get("start", 0)
+
+    try:
+        seek_pos = long(seek_pos)
+    except ValueError:
+        seek_pos = video_manager.get_relative_mp4(seek_pos)
+
+    if seek_pos > 0 and manage.videoManager.suportaSeekBar():
+        manage.setRandomRead(seek_pos)
+        status_code = 206
+    else:
+        manage.reloadSettings()
+        status_code = 200
+
+    filename = manage.getVideoTitle()
+    filename = filename.encode("utf-8", "ignore")
+    video_size = manage.getVideoSize()
+    video_type = manage.getVideoExt()
+
+    response = StreamingHttpResponse(manage.get_streamer(), status=status_code)
+    response["Content-Type"] = "video/%s" % video_type
+    response["Content-Length"] = video_size
+    response["Content-Transfer-Encoding"] = "binary"
+    response['Content-Disposition'] = 'attachment; filename=%s' % filename
+    response["Accept-Ranges"] = "bytes"
+    return response
 
 
-
-
-
+def player_loader(request):
+    params = request.GET.copy()
+    for key in params: params[key] = urllib.unquote_plus(params[key])
+    return render_to_response(params["template"], {"params": params},
+                              mimetype="text/html")
