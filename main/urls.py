3bb456cfe838539cfda103b4df9d09f30425199c
alex <geniofuturo@gmail.com>
2014-02-23 00:30:36 -0300

    carregando arquivos estáticos

--- main/urls.py
+++ main/urls.py
@@ -5,17 +5,18 @@ from .app import views
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.conf import settings
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
-                       # url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
-                       #     {
-                       #         'document_root': settings.STATIC_PATH,
-                       #         'show_indexes': True
-                       #     }
-                       # ),
+                       url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+                           {
+                               'document_root': settings.STATIC_PATH,
+                               'show_indexes': True
+                           }
+                       ),
                        url(r'^stream/.+', views.stream_loader),
                        url(r'^player/', views.player_loader, name='player-loader')
 )
