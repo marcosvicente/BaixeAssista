5937e76113290ac9e8d900a31eddbc9f786895b8
alex <geniofuturo@gmail.com>
2014-03-01 20:01:14 -0300

    Simplificando info.

--- main/app/manager/info.py
+++ main/app/manager/info.py
@@ -11,23 +11,12 @@ class Info(object):
     # 'Signal' usado para notificar atualização dos dados de I/O
     ##
     update = Signal(providing_args=["fields"])
+    lock = threading.RLock()
     update_sleep = 0.05
     info_timer = {}
     info = {}
 
-    class Synchronize(object):
-        """ Sicroniza as alterações sobre 'info' nas diferentes threads """
-        lock = threading.RLock()
-
-        def __init__(self, func):
-            self.func = func
-
-        def __call__(self, *args, **kwargs):
-            with self.lock:
-                return self.func(*args, **kwargs)
-
     @classmethod
-    @Synchronize
     def send(cls, *args, **kwargs):
         if len(kwargs["fields"]) == 1:
             field = kwargs["fields"][0]
@@ -37,35 +26,28 @@ class Info(object):
         cls.update.send(*args, **kwargs)
 
     @classmethod
-    @Synchronize
     def add(cls, ident):
         cls.info[ident] = {}
 
     @classmethod
-    @Synchronize
     def delete(cls, ident):
         return cls.info.pop(ident, None)
 
     @classmethod
-    @Synchronize
     def get(cls, ident, name):
-        if ident in cls.info:
-            items = cls.info[ident]
-        else:
-            items = {}
-        return items.get(name, '')
+        return cls.info[ident].get(name, '') if ident in cls.info else ''
 
     @classmethod
-    @Synchronize
     def set(cls, ident, name, value):
-        cls.info[ident][name] = value
-        cls.info_timer.setdefault(name, time.time())
-        cls.send(ident, fields=(name,))
+        with cls.lock:
+            cls.info[ident][name] = value
+            cls.info_timer.setdefault(name, time.time())
+            cls.send(ident, fields=(name,))
 
     @classmethod
-    @Synchronize
     def clear(cls, ident, *names, **params):
-        for name in (names or cls.info[ident]):
-            if not name in params.get("exclude", []):
-                cls.info[ident][name] = ''
-        cls.send(ident, fields=names)
+        with cls.lock:
+            for name in (names or cls.info[ident]):
+                if not name in params.get("exclude", []):
+                    cls.info[ident][name] = ''
+            cls.send(ident, fields=names)
